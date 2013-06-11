e7ffb0912e247cb1b366cd676eaef69e42094a90
gatorsarah <gatorsarah@gmail.com>
2013-06-11 19:08:34 -0400

    another test

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -5,7 +5,10 @@ class HistoryController < ApplicationController
 	def View
  
 	 	@actionHistory = [ActionHistory.new("Initialize", "sclaus", DateTime.now) , ActionHistory.new("Activated", "ebunny", DateTime.now), ActionHistory.new("Exploded", "jfrost", DateTime.now)]
-
+    
+    head :ok
+    
+    
 	end
 
 end
