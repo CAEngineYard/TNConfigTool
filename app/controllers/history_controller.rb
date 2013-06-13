bbe214d89dd1322331f649b1230464f69c6c671e
gatorsarah <gatorsarah@gmail.com>
2013-06-13 16:57:54 -0400

    Commenting out the okta call

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -3,10 +3,10 @@ require 'ActionHistory'
 class HistoryController < ApplicationController
 
 	def View
-    if session[:userid].nil?
-      #session[:goback_to] = request.url
-      session[:userid]=nil
-      return redirect_to '/saml'
+  #  if session[:userid].nil?
+  #    #session[:goback_to] = request.url
+   #   session[:userid]=nil
+  #    return redirect_to '/saml'
 
     end
 	 	@actionHistory = [ActionHistory.new("Initialize", "sclaus", DateTime.now) , ActionHistory.new("Activated", "ebunny", DateTime.now), ActionHistory.new("Exploded", "jfrost", DateTime.now)]
