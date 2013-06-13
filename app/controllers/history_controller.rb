55de5edb9340b50932a9f04f66d2a563d3870b53
gatorsarah <gatorsarah@gmail.com>
2013-06-13 18:07:07 -0400

    renaming view and trying this again

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -2,13 +2,13 @@ require 'ActionHistory'
 
 class HistoryController < ApplicationController
 
-	def View
-  #  if session[:userid].nil?
-  #    #session[:goback_to] = request.url
-   #   session[:userid]=nil
-  #    return redirect_to '/saml'
+	def view
+    if session[:userid].nil?
+      #session[:goback_to] = request.url
+      session[:userid]=nil
+     return redirect_to '/saml'
 
-   # end
+    end
 	 	@actionHistory = [ActionHistory.new("Initialize", "sclaus", DateTime.now) , ActionHistory.new("Activated", "ebunny", DateTime.now), ActionHistory.new("Exploded", "jfrost", DateTime.now)]
     
     render "View.html.erb", :layout => true
